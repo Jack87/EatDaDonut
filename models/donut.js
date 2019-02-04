9edc1ab0f1438bd2760b7c2812602228ebc50bc4
Akop K <akop87@gmail.com>
2019-02-04 00:59:38 -0700

    working donut munch list

--- models/donut.js
+++ models/donut.js
@@ -1,23 +1,23 @@
 var orm = require("../config/orm.js");
 
 var donut = {
-    all: function(cb) {
+  selectAll: function(cb) {
       orm.selectAll("donuts", function(res) {
         cb(res);
       });
     },
     // The variables cols and vals are arrays.
-    create: function(cols, vals, cb) {
+    insertOne: function(cols, vals, cb) {
       orm.insertOne("donuts", cols, vals, function(res) {
         cb(res);
       });
     },
-    update: function(objColVals, condition, cb) {
+    updateOne: function(objColVals, condition, cb) {
       orm.updateOne("donuts", objColVals, condition, function(res) {
         cb(res);
       });
     },
-    delete: function(condition, cb) {
+    deleteOne: function(condition, cb) {
       orm.deleteOne("donuts", condition, function(res) {
         cb(res);
       });
